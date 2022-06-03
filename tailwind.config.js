fda8739713901b0f6cbec211a53d931180c1f78e
Marko Antolovic <bronhy@gmail.com>
2022-06-03 16:54:58 +0200

    Add old build

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      'koromiko': '#FFCA6E',
+      'kabul': '#664842',
+      'fall-green': '#ECE4B7',
+      'sorrell-brown': '#C8AB83',
+      'rock-blue': '#9EADC8'
+    },
+    fontFamily: {
+      'sans': ['"Source Sans Pro"', 'sans-serif']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
